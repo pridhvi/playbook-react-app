940f75ccec149d506436b66d77d1fcf84de31154
pridhvi <pridhvimuthuraju@gmail.com>
2023-04-04 22:28:18 -0400

    search bar: truncated placeholder, icon to front

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -75,22 +75,22 @@ const Search: React.FC<SearchProps> = ({}) => {
       <div className="row d-flex justify-content-center">
         {/* Search Bar */}
         <div className="col-12 container">
-          <div className="container p-0 col-10 col-lg-7 col-xl-5 m-auto rounded-pill bg-black d-flex align-items-center border">
-            {/* <i className="bi bi-search text-white d-inline position-absolute ms-3"></i> */}
-            <form onSubmit={searchSubmitHandler} className="w-100">
+          <div className="container p-0 col-10 col-lg-7 col-xl-5 col-xxl-4 m-auto rounded-pill bg-black d-flex align-items-center border">
+            <i className="bi bi-search text-white d-inline position-absolute ms-3"></i>
+            <form onSubmit={searchSubmitHandler} className="w-100 ms-4">
               <input
                 type="text"
-                className="text-white form-control rounded-pill form-control-lg border-0 d-inline shadow-none bg-black"
+                className="text-truncate text-white form-control rounded-pill form-control-lg border-0 d-inline shadow-none bg-black"
                 id="search"
                 // placeholder="Search Playbook"
-                placeholder="Search a game, character and much more..."
+                placeholder="Search games, characters, and companies"
                 value={criteria}
                 onChange={criteriaChangeHandler}
               />
-              <button
+              {/* <button
                 type="submit"
                 className="p-2 ps-3 pe-3 rounded-circle bi bi-search text-white position-absolute wb-search-icon"
-              ></button>
+              ></button> */}
             </form>
           </div>
         </div>
